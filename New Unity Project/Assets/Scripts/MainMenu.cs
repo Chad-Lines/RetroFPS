d7e3f0571e7319134628df480174b281f29ce71e
Chad <chad.lines1@gmail.com>
2019-10-09 17:41:22 -0600

    11.003 Start and Quit Scripts

---
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Allows us to switch scenes
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string firstLevel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(firstLevel);
+    }
+
+    public void OpenOptions()
+    {
+
+    }
+
+    public void CloseOptions()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        // Quit the game
+        Application.Quit();
+    }
+}
